598f0c7f9323446a08c66be39d5e459743515870
Francisco Fourcade <francisco.fourcade@openbravo.com>
2025-03-10 23:35:39 -0300

    Import/Export

---
+++ src/components/ImportExport.jsx
@@ -0,0 +1,71 @@
+export  function ImportExport() {
+  return (
+    <>
+      <h4>Importing and exporting components</h4>
+      <span>We can define as many components as we want inside a single file, but creating different files for different components will make them more modular and reusable in other files</span>
+      <h4>Export/Import Syntax Reference</h4>
+      <ExportImportTable />
+    </>
+  )
+}
+
+const ExportImportTable = () => {
+  const tableStyle = {
+    width: '100%',
+    borderCollapse: 'collapse',
+    backgroundColor: 'lightgrey',
+  };
+
+  const thTdStyle = {
+    border: '1px solid #475569', // Dark gray border
+    padding: '10px',
+    textAlign: 'left',
+  };
+
+  const codeStyle = {
+    backgroundColor: 'darkgrey', 
+    padding: '4px 8px',
+    borderRadius: '4px',
+    display: 'inline-block',
+    fontFamily: 'monospace',
+  };
+
+  const containerStyle = {
+    overflowX: 'auto',
+    padding: '0px',
+  };
+
+  return (
+    <div style={containerStyle}>
+      <table style={tableStyle}>
+        <thead>
+          <tr>
+            <th style={thTdStyle}>Syntax</th>
+            <th style={thTdStyle}>Export statement</th>
+            <th style={thTdStyle}>Import statement</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td style={thTdStyle}>Default</td>
+            <td style={thTdStyle}>
+              <code style={codeStyle}>export default function Button() {'{}'}</code>
+            </td>
+            <td style={thTdStyle}>
+              <code style={codeStyle}>import Button from './Button.js';</code>
+            </td>
+          </tr>
+          <tr>
+            <td style={thTdStyle}>Named</td>
+            <td style={thTdStyle}>
+              <code style={codeStyle}>export function Button() {'{}'}</code>
+            </td>
+            <td style={thTdStyle}>
+              <code style={codeStyle}>import {'{ Button }'} from './Button.js';</code>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
